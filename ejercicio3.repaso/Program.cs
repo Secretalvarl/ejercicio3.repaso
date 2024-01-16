3ff74d23309f746b4d2e36ed263091fc03045bc3
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-01-16 13:34:21 +0100

    Agregar archivos de proyecto.

---
+++ ejercicio3.repaso/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ejercicio3.repaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribame cuanto pesa el saco, dimelo con un numero entero");
+            int numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+            int capacidadMaxima = 4000;
+            
+            int numeroDeSacos = capacidadMaxima / numeroEntero ;
+
+            Console.WriteLine("El numero de sacos que puede cargar el camion es: " + numeroDeSacos);
+
+        }
+    }
+}
